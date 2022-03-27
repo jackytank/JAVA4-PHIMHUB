2fb7d0c75957fa6dfeeaffeb8cd5edec22bcfa98
jackytank <balisongian@gmail.com>
2022-03-27 22:42:11 +0700

    chỉnh sửa TestVideoDAO
    thêm expectedException ở một vài test case

--- assm-user/src/test/TestVideoDAO.java
+++ assm-user/src/test/TestVideoDAO.java
@@ -6,7 +6,7 @@ import dao.VideoDAO;
 import model.Video;
 
 public class TestVideoDAO {
-	@Test
+	@Test(expectedExceptions = IllegalArgumentException.class)
 	public void testInsert1() {
 		VideoDAO dao = new VideoDAO();
 		Video video = new Video();
@@ -18,7 +18,7 @@ public class TestVideoDAO {
 		video.setShares(null);
 		video.setTitle(null);
 
-		dao.insert(null);
+		dao.insert(video);
 	}
 
 	@Test
@@ -32,10 +32,10 @@ public class TestVideoDAO {
 		video.setPoster("Mancity");
 		video.setShares(null);
 		video.setTitle("VIP");
-		
+
 		dao.insert(video);
 	}
-	
+
 	@Test
 	public void testInsert3() {
 		VideoDAO dao = new VideoDAO();
@@ -48,96 +48,96 @@ public class TestVideoDAO {
 		video.setShares(null);
 		video.setTitle(null);
 
-		dao.insert(null);
+		dao.insert(video);
 	}
-	
-	@Test
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
 	public void testDelete1() {
 		VideoDAO dao = new VideoDAO();
 		Video video = new Video();
-		
+
 		video.setVideoId(null);
 		video.setDescription(null);
 		video.setFavorites(null);
 		video.setPoster(null);
 		video.setShares(null);
 		video.setTitle(null);
-		
-		dao.delete(null);
+
+		dao.delete(video);
 	}
-	
-	@Test
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
 	public void testDelete2() {
 		VideoDAO dao = new VideoDAO();
 		Video video = new Video();
-		
+
 		video.setVideoId("VD02");
 		video.setDescription("vietnammuionnam");
 		video.setFavorites(null);
 		video.setPoster("VN");
 		video.setShares(null);
 		video.setTitle("VIP1");
-		
+
 		dao.delete(video);
 	}
-	
+
 	@Test
 	public void testDelete3() {
 		VideoDAO dao = new VideoDAO();
 		Video video = new Video();
-		
-		video.setVideoId(null);
+
+		video.setVideoId("VD01");
 		video.setDescription("12312");
 		video.setFavorites(null);
 		video.setPoster("23123123");
 		video.setShares(null);
 		video.setTitle("2222112");
-		
-		dao.delete(null);
+
+		dao.delete(video);
 	}
-	
-	@Test
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
 	public void testUpdate1() {
 		VideoDAO dao = new VideoDAO();
 		Video video = new Video();
-		
+
 		video.setVideoId(null);
 		video.setDescription(null);
 		video.setFavorites(null);
 		video.setPoster(null);
 		video.setShares(null);
 		video.setTitle(null);
-		
-		dao.update(null);
+
+		dao.update(video);
 	}
-	
+
 	@Test
 	public void testUpdate2() {
 		VideoDAO dao = new VideoDAO();
 		Video video = new Video();
-		
+
 		video.setVideoId("VD03");
 		video.setDescription("tamnt");
 		video.setFavorites(null);
 		video.setPoster("161123123");
 		video.setShares(null);
 		video.setTitle("VIP1213123");
-		
+
 		dao.update(video);
 	}
-	
+
 	@Test
 	public void testUpdate3() {
 		VideoDAO dao = new VideoDAO();
 		Video video = new Video();
-		
+
 		video.setVideoId("123");
 		video.setDescription("22222");
 		video.setFavorites(null);
 		video.setPoster(null);
 		video.setShares(null);
 		video.setTitle("2121212");
-		
-		dao.update(null);
+
+		dao.update(video);
 	}
 }
