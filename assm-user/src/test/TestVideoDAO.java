74953b74cd9d428ed6fe82fb613bf3cde41861c0
tamntps15500 <81551934+tamntps15500@users.noreply.github.com>
2022-03-27 15:28:27 +0700

    Test VideoDAO (Tam)

--- assm-user/src/test/TestVideoDAO.java
+++ assm-user/src/test/TestVideoDAO.java
@@ -2,6 +2,142 @@ package test;
 
 import org.testng.annotations.Test;
 
-public class TestVideoDAO {
+import dao.VideoDAO;
+import model.Video;
 
+public class TestVideoDAO {
+	@Test
+	public void testInsert1() {
+		VideoDAO dao = new VideoDAO();
+		Video video = new Video();
+
+		video.setVideoId(null);
+		video.setDescription(null);
+		video.setFavorites(null);
+		video.setPoster(null);
+		video.setShares(null);
+		video.setTitle(null);
+
+		dao.insert(null);
+	}
+
+	@Test
+	public void testInsert2() {
+		VideoDAO dao = new VideoDAO();
+		Video video = new Video();
+
+		video.setVideoId("VD01");
+		video.setDescription("abcfh");
+		video.setFavorites(null);
+		video.setPoster("Mancity");
+		video.setShares(null);
+		video.setTitle("VIP");
+		
+		dao.insert(video);
+	}
+	
+	@Test
+	public void testInsert3() {
+		VideoDAO dao = new VideoDAO();
+		Video video = new Video();
+
+		video.setVideoId(null);
+		video.setDescription("xzxas");
+		video.setFavorites(null);
+		video.setPoster("ádasdacwc");
+		video.setShares(null);
+		video.setTitle(null);
+
+		dao.insert(null);
+	}
+	
+	@Test
+	public void testDelete1() {
+		VideoDAO dao = new VideoDAO();
+		Video video = new Video();
+		
+		video.setVideoId(null);
+		video.setDescription(null);
+		video.setFavorites(null);
+		video.setPoster(null);
+		video.setShares(null);
+		video.setTitle(null);
+		
+		dao.delete(null);
+	}
+	
+	@Test
+	public void testDelete2() {
+		VideoDAO dao = new VideoDAO();
+		Video video = new Video();
+		
+		video.setVideoId("VD02");
+		video.setDescription("vietnammuionnam");
+		video.setFavorites(null);
+		video.setPoster("VN");
+		video.setShares(null);
+		video.setTitle("VIP1");
+		
+		dao.delete(video);
+	}
+	
+	@Test
+	public void testDelete3() {
+		VideoDAO dao = new VideoDAO();
+		Video video = new Video();
+		
+		video.setVideoId(null);
+		video.setDescription("12312");
+		video.setFavorites(null);
+		video.setPoster("23123123");
+		video.setShares(null);
+		video.setTitle("2222112");
+		
+		dao.delete(null);
+	}
+	
+	@Test
+	public void testUpdate1() {
+		VideoDAO dao = new VideoDAO();
+		Video video = new Video();
+		
+		video.setVideoId(null);
+		video.setDescription(null);
+		video.setFavorites(null);
+		video.setPoster(null);
+		video.setShares(null);
+		video.setTitle(null);
+		
+		dao.update(null);
+	}
+	
+	@Test
+	public void testUpdate2() {
+		VideoDAO dao = new VideoDAO();
+		Video video = new Video();
+		
+		video.setVideoId("VD03");
+		video.setDescription("tamnt");
+		video.setFavorites(null);
+		video.setPoster("161123123");
+		video.setShares(null);
+		video.setTitle("VIP1213123");
+		
+		dao.update(video);
+	}
+	
+	@Test
+	public void testUpdate3() {
+		VideoDAO dao = new VideoDAO();
+		Video video = new Video();
+		
+		video.setVideoId("123");
+		video.setDescription("22222");
+		video.setFavorites(null);
+		video.setPoster(null);
+		video.setShares(null);
+		video.setTitle("2121212");
+		
+		dao.update(null);
+	}
 }
