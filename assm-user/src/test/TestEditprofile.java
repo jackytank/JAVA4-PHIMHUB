19ad704d399faeb85dd7d92a60ea36d3ef6db117
jackytank <balisongian@gmail.com>
2022-04-12 11:12:40 +0700

    push testEditprofile

--- assm-user/src/test/TestEditprofile.java
+++ assm-user/src/test/TestEditprofile.java
@@ -20,7 +20,7 @@ import utils.ExcelUtil;
 public class TestEditprofile {
 	String url = "http://localhost:8080/assm-user/EditProfile";
 	public WebDriver driver;
-	
+
 	@SuppressWarnings("deprecation")
 	@BeforeMethod
 	public void launchBrowser() throws Exception {
@@ -31,35 +31,44 @@ public class TestEditprofile {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get(url);
 	}
+
 	@Test(dataProvider = "EditProFileProvider")
 	public void testLogin1(String username, String password, String newpassword, String fullname, String email) {
 		try {
 			Thread.sleep(500);
 			System.out.println("testLogin1 running...");
-			// sending test Input data
+			// sending test input data to login page
+			driver.findElement(By.name("userId")).sendKeys(username);
+			System.out.println(username);
+			driver.findElement(By.name("password")).sendKeys(password);
+			System.out.println(password);
+			driver.findElement(By.name("loginBtn")).click();
+			driver.get(url);
+			// sending test Input data to edit profile page
 			driver.findElement(By.name("userId")).sendKeys(username);
 			System.out.println(username);
 			driver.findElement(By.name("password")).sendKeys(newpassword);
 			System.out.println(password);
 			driver.findElement(By.name("fullname")).sendKeys(fullname);
-			System.out.println(fullname);	
+			System.out.println(fullname);
 			driver.findElement(By.name("email")).sendKeys(email);
 			System.out.println(email);
 			driver.findElement(By.name("updateBtn")).click();
-			
+
 			Thread.sleep(500);
 			// check login result by checking page title
-			String expectedTitle = "Home";
+			String expectedTitle = "Profile";
 			String actualTitle = driver.getTitle();
 			assertEquals(expectedTitle, actualTitle);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	@DataProvider
 	public Object[][] EditProFileProvider() throws Exception {
-		String excelFilePath = Paths.get("src", "resources", "EditprofileData.xlsx").toFile().getAbsolutePath(); 
-		Object[][] arr  = ExcelUtil.getTableArray(excelFilePath, "Sheet1", 5);
+		String excelFilePath = Paths.get("src", "resources", "EditprofileData.xlsx").toFile().getAbsolutePath();
+		Object[][] arr = ExcelUtil.getTableArray(excelFilePath, "Sheet1", 5);
 		return arr;
 	}
 
